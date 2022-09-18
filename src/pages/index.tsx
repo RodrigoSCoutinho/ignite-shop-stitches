09da0ed774dd547f100ebfb0074ee497adb69b91
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2022-09-18 19:53:10 -0300

    add the images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
-import { styled } from '../../styles'
-
-const Button = styled('button', {
-  backgroundColor: '$rocketseat'
-})
+import { HomeContainer, Product } from '../../styles/pages/home'
+import Image from 'next/future/image'
+import tshirt1 from '../assets/tshirts/tshirt1.png'
+import tshirt2 from '../assets/tshirts/tshirt2.png'
+import tshirt3 from '../assets/tshirts/tshirt3.png'
 
 export default function Home() {
   return (
-      <Button>Hello World</Button>
+      <HomeContainer>
+        <Product>
+          <Image src={tshirt1} alt={''} width={520} height={480}/>
+
+          <footer>
+            <strong> T-shirt X</strong>
+            <span>  R$: 79,90</span>
+          </footer>
+        </Product>
+      </HomeContainer>
   )
 }
---
+++ styles/pages/home.ts
@@ -0,0 +1,12 @@
+import { styled } from "..";
+
+export const HomeContainer = styled('main',{
+  display: 'flex',
+  gap: '3rem',
+  maxWidth: 'calc(100vw - ((100vw 1180px) /2))'
+})
+
+
+export const Product= styled('a',{
+
+})
