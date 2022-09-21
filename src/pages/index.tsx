32c09397725e1e6ec28e19d31c035b38321fe783
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2022-09-21 07:24:50 -0300

    add the configs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
 import { HomeContainer, Product } from '../../styles/pages/home'
 import Image from 'next/future/image'
+import { useKeenSlider } from 'keen-slider/react'
 import tshirt1 from '../assets/tshirts/tshirt1.png'
 import tshirt2 from '../assets/tshirts/tshirt2.png'
 import tshirt3 from '../assets/tshirts/tshirt3.png'
 
+import 'keen-slider/keen-slider.min.css';
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-      <HomeContainer>
-        <Product>
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        <Product className="keen-slider__slide">
           <Image src={tshirt1} alt={''} width={520} height={480}/>
 
           <footer>
@@ -15,8 +25,36 @@ export default function Home() {
             <span>  R$: 79,90</span>
           </footer>
         </Product>
-        <Product>
-          <Image src={tshirt2} alt={''} width={520} height={480}/>
+      
+        <Product className="keen-slider__slide">
+          <Image src={tshirt1} alt={''} width={520} height={480}/>
+
+          <footer>
+            <strong> T-shirt X</strong>
+            <span>  R$: 79,90</span>
+          </footer>
+        </Product>
+      
+        <Product className="keen-slider__slide">
+          <Image src={tshirt1} alt={''} width={520} height={480}/>
+
+          <footer>
+            <strong> T-shirt X</strong>
+            <span>  R$: 79,90</span>
+          </footer>
+        </Product>
+
+        <Product className="keen-slider__slide">
+          <Image src={tshirt1} alt={''} width={520} height={480}/>
+
+          <footer>
+            <strong> T-shirt X</strong>
+            <span>  R$: 79,90</span>
+          </footer>
+        </Product>
+
+        <Product className="keen-slider__slide">
+          <Image src={tshirt1} alt={''} width={520} height={480}/>
 
           <footer>
             <strong> T-shirt X</strong>
